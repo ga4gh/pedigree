6b0ce5699a37176dc6da9e75a907d2365dea30b3
Orion Buske <orion.buske@gmail.com>
2022-11-03 09:50:43 -0400

    Add custom css to wrap text in tables

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,3 +34,12 @@ html_logo = 'images/GA-logo.png'
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
